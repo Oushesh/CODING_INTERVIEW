88920f87d45e5ec588fb64759680ed73781c3fff
oushesh <oushesh@gmail.com>
2020-10-23 22:58:01 +0200

    Updated LandandSea_Second Version
    oushesh
    
    Co-Authored-By: Oushesh Haradhun <oushesh@gmail.com>

--- LeetCode/LandandSea_2.py
+++ LeetCode/LandandSea_2.py
@@ -6,8 +6,14 @@ We modify the code into classical approach of DFS:
 '''
 
 class Islands_Graphs():
-
+    def grid2graph(self,grid):
+        graph = {}
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                graph[grid[i]]=
 
 
 
 if __name__ == '__main__':
+    grid = [['1','2','3','4'],['5','6','7','8'],['9','10','11','12']]
+    #Build adjacency graph
