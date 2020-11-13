ee3366d86850dc156e8a2303f6ede20ef65e7c81
oushesh <oushesh@gmail.com>
2020-11-13 15:15:20 +0100

    TODO: Google Engineer Walls and Gates Problem
    Google Engineer Walls and Gates Problem

---
+++ interviewing.io/GoogleEngineer_Walls_and_Gates.py
@@ -0,0 +1,20 @@
+'''
+Leetcode Link: https://leetcode.com/discuss/interview-question/algorithms/124722/facebook-phone-screen-walls-and-gates
+Interviewing.io Link: https://interviewing.io/recordings/Java-Google-22
+Asked at Facebook Phone Screen interview. Can also be asked at Google
+Phone Screen Interview
+'''
+
+class Problem():
+    def __init__:
+        self.property1 = property1
+        self.property2 = property2
+
+    def firstfuntion(self,input):
+        return output
+
+if __name__ == '__main__':
+    space = [[]]
+    object = Problem()
+    input = .....
+    print ('The solution to the given problem is:',object.firstfunction(input))
