904daeab6aafa404a7f0e45210531ec51db8c745
oushesh <oushesh@gmail.com>
2021-01-21 14:38:00 +0100

    xTotalShapes
    1. Added xTotalShapes.cpp which ressemble the islands and land problem.
    
    Co-Authored-By: Oushesh Haradhun <oushesh@gmail.com>

--- GeeksforGeeks/xTotalShapes.cpp
+++ GeeksforGeeks/xTotalShapes.cpp
@@ -7,29 +7,58 @@ Reference: https://practice.geeksforgeeks.org/problems/x-total-shapes3617/1/?com
    1. Loop over the grid & spot the first:
       Lets say we count X->1, 0->0
 
-
-      We loop over the 
+      We loop over the neighbours &
 */
 #include <iostream>
 #include <vector>
-#include <arithmetic>
 
 using namespace std;
 
+//A different style of solving it is to feed (i,j)--> output the neighbours
+//as 4 tuples of the vector data structure then for all neighbour in neighbours:
+//perform the 4 function calls.
+
+void neighbours(vector <vector<int>> &grid,int i, int j, int m, int n)
+{
+  if (i<0 || j<0 || i>m || j>n || grid[i][j]!=1)
+    return;
+
+  //once we are in the function:
+  //each time when we start we can perform recursive call.
+  grid[i][j]=0;
+
+  neighbours(grid,i+1,j,m,n);
+  neighbours(grid,i-1,j,m,n);
+  neighbours(grid,i,j+1,m,n);
+  neighbours(grid,i,j-1,m,n);
+}
 
 int count(vector<vector<int>> &grid)
 {
-  count = 0;
+  int count = 0;
+  int m = grid.size();
+  int n = grid[0].size();
 
-  neighbours(int i, int j);
+  //Loop over the whole grid.
+  for (int i=0;i<m;i++)
+  {
+    for (int j=0;j<n;j++)
+    {
+      if (grid[i][j]==1)
+        {
+          neighbours(grid,i,j,m,n);
+          count+=1;
+        }
+    }
+  }
   return count;
-
 }
 
-
+//The number of islands 1s is 3.
 int main()
 {
   vector<vector<int>> grid = {{1,0,1},{0,1,0},{1,1,1}};
-  cout << "The number of islands is:" << count(grid) << endl;
+  cout << "ABC";
+  cout << "The number of islands is:" << count(grid);
   return 0;
 }
