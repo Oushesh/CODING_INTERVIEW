97a31e27832f8f67ee7cc7d6de85e824c6560c28
oushesh <oushesh@gmail.com>
2021-02-10 13:00:59 +0100

    added 2 approaches for Palindrome string

--- InterviewCake/Python/DynamicProgramming/palindrome.py
+++ InterviewCake/Python/DynamicProgramming/palindrome.py
@@ -22,10 +22,38 @@ What about this case? --> 'banna'
 1. 2 pointers (left, right).
 left = 0, right = len(string)
 while (left<=right):
-    check_palindrome(substring)
-    left+=1
-    if not check_palindrome:
+    while  check_palindrome(substring):
+        output.append(substring)
+        left+=1
         right-=1
-        check_palindrome
-
+    #If not palindrome then test either by moving left or right individually at one time
+    if not check_palindrome:
+        left+=1
+    if not checkpalindrome(substring):
+        right-=1
 '''
+
+class String():
+    def palindrome
+    def check(self,string):
+        output = []
+        left = 0
+        right = len(string)
+
+        #The biggest value of a coder is to fix his own bugs in thinking
+        while (right>left):
+            if (self.check(string[left:right])):
+                output.append(string[left:right])
+                left+=1
+                right-=1
+            if not self.check(string[left:right]):
+                left+=1
+            if not self.check(string[left:right]):
+                right-=1
+        return output
+
+
+if __name__ == "__main__":
+    string = "anna"
+    current_palindrome = Palindrome()
+    print ('The palindromes from this string are:', current_palindrome.check(string))
