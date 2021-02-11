6ac5315e9a23356ee131a13fa14b335d49a9851c
oushesh <oushesh@gmail.com>
2021-02-11 20:41:45 +0100

    added Answer to Constructors in C++

---
+++ EpicGames/Q03.cpp
@@ -0,0 +1,10 @@
+/*
+
+Write the code for the following function, without using any built-in functions
+except malloc or operator new..
+char* itoa(int Value, int Base);
+
+where the returned value is allocated on behalf of the caller, value is
+the integer to convert, and base is octal, decimal, or hex.
+Your implementation is expected to be robust and production ready
+*/
