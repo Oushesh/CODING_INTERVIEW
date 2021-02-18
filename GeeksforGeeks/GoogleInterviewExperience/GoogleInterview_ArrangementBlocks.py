d29eeecc3d78cbc97a8aa67dd1ce994c0409b2e1
oushesh <oushesh@gmail.com>
2021-02-18 13:46:33 +0100

    Google Interview Rearrange Blocks so once see a fixed number from Left && another number from Right

--- GeeksforGeeks/GoogleInterviewExperience/GoogleInterview_ArrangementBlocks.py
+++ GeeksforGeeks/GoogleInterviewExperience/GoogleInterview_ArrangementBlocks.py
@@ -31,12 +31,53 @@ Can we do better? What if while
 Example: 1,2,3,4,5,6
          from a sorted list we take n highest
          5,6,--,--,--,--
-                      
-
 
 leftmost 2 --> every number else has to be 3, rightmost
-
 [2,]
 This is an arrangement problem. or sorting problem
 '''
+
+
+#Let's do it for Google!
+class arrangement:
+    def __init__(self,L,R,blocks):
+        self.L = L
+        self.R = R
+        self.blocks = blocks
+
+    def rearrange(self):
+        #Edge case: L and R cannot be the same
+        assert(not L==R)
+        #The other edge case is also to check
+        assert (L+R == len(self.blocks))
+
+        if (not blocks==sorted(blocks)):
+            blocks = sorted(blocks) #sorted with the min on the left and max on the right
+
+        '''
+        if L=2, R=1 --> From right I can only seen 1 block across., From left
+        I can only see 2 blocks across.
+        '''
+        #We take the minimum from L and R
+        minimum = min(L,R)
+        #second is just not the min.
+        second = max(L,R)
+        second-=1
+
+        #Write a way to write number of combinations
+
+
+
+
+
 if __name__ == "__main__":
+    L =
+    R =
+    blocks
+
+    #Unit test check input
+    assert(L==int)
+    assert(R==int)
+    assert(is_instance(blocks,list))
+
+    print ('The number of arrangements so one can see blocks based on the constraint given is:',)
